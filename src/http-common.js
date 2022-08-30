082fe6ef7bad04d6cc31160f7c6dfbe1dc9237ba
Paulo Silva <52604868+paulodevsilva@users.noreply.github.com>
2022-08-30 04:46:29 -0300

    service config

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3333/api',
+  baseURL: 'https://crud-qualicorp.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
